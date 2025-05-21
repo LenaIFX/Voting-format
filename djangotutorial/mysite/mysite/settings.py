d076f047d4206f9a7bdcc9e4d662bd418de8a6bc
LenaIFX <lena.jestin@infineon.com>
2025-05-21 09:33:43 +0200

    WebsiteTest

--- djangotutorial/mysite/mysite/settings.py
+++ djangotutorial/mysite/mysite/settings.py
@@ -15,15 +15,22 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file if it exists
+env_path = os.path.join(BASE_DIR, '.env')
+load_dotenv(env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&#x1h!r3g(l7zpkz1t9-gbxoq$5x669w*#@%w(s)%kmo2=2ofo'
+# SECRET_KEY = 'django-insecure-&#x1h!r3g(l7zpkz1t9-gbxoq$5x669w*#@%w(s)%kmo2=2ofo'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&#x1h!r3g(l7zpkz1t9-gbxoq$5x669w*#@%w(s)%kmo2=2ofo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
