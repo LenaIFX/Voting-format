2856384ee39924dca6814856ffdb01a3e5792a85
LenaIFX <lena.jestin@infineon.com>
2025-05-22 13:16:15 +0200

    commit#8 22.05 -admin

--- survey/views.py
+++ survey/views.py
@@ -7,6 +7,12 @@ import pandas as pd
 import os
 from django.contrib import messages
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "yourpassword")
+    
 def register_user(request):
     if request.method == 'POST':
         form = UserInfoForm(request.POST)
