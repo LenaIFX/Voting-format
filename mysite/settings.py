ec1e16a57ab78e272c73d2bc5c0a205ddd7f03ab
LenaIFX <lena.jestin@infineon.com>
2025-05-22 10:56:26 +0200

    commit#5 22.05

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&#x1h!r3g(l7zpkz1t9-gbxoq$5x669w*#@%w(s)%kmo2=2ofo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
