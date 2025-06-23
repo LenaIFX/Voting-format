60622a86b2a627583d1002f9644f280b0af58668
LenaIFX <lena.jestin@infineon.com>
2025-06-23 16:24:58 +0200

    commit#8

---
+++ create_superuser.py
@@ -0,0 +1,18 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+username = "admin"
+email = "admin@example.com"
+password = "your_secure_password"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
